1eaf95acd4a60d21bd2090ef51c5895d0c0ceda4
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2014-06-23 09:27:19 +0530

    Refactor and format.

--- src/main/java/org/scribe/builder/api/LinksApi.java
+++ src/main/java/org/scribe/builder/api/LinksApi.java
@@ -7,41 +7,41 @@ import org.scribe.utils.*;
 /**
  * LinksApi open platform api based on OAuth 2.0.
  */
-public class LinksApi extends DefaultApi20
-{
-
-  private static final String AUTHORIZE_URL = "http://links.t.proxylocal.com/oauth/authorize?client_id=%s&redirect_uri=%s&response_type=code";
-  private static final String SCOPED_AUTHORIZE_URL = AUTHORIZE_URL + "&scope=%s";
+public class LinksApi extends DefaultApi20 {
+  private static final String BASE_URL = "http://192.168.13.26:3000";
+  
+  private static final String AUTHORIZE_URL = BASE_URL
+      + "/oauth/authorize?client_id=%s&redirect_uri=%s&response_type=code";
+  private static final String SCOPED_AUTHORIZE_URL = AUTHORIZE_URL
+      + "&scope=%s";
+  private static final String ACCESS_TOKEN_ENDPOINT = BASE_URL
+      + "/oauth/token?grant_type=authorization_code";
 
   @Override
-  public AccessTokenExtractor getAccessTokenExtractor()
-  {
+  public AccessTokenExtractor getAccessTokenExtractor() {
     return new JsonTokenExtractor();
   }
 
   @Override
-  public String getAccessTokenEndpoint()
-  {
-    return "http://links.t.proxylocal.com/oauth/token?grant_type=authorization_code";
+  public String getAccessTokenEndpoint() {
+    return ACCESS_TOKEN_ENDPOINT;
   }
 
   @Override
-  public Verb getAccessTokenVerb()
-  {
+  public Verb getAccessTokenVerb() {
     return Verb.POST;
   }
-  
+
   @Override
-  public String getAuthorizationUrl(OAuthConfig config)
-  {
+  public String getAuthorizationUrl(OAuthConfig config) {
     // Append scope if present
-    if (config.hasScope())
-    {
-      return String.format(SCOPED_AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()), OAuthEncoder.encode(config.getScope()));
-    }
-    else
-    {
-      return String.format(AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()));
+    if (config.hasScope()) {
+      return String.format(SCOPED_AUTHORIZE_URL, config.getApiKey(),
+          OAuthEncoder.encode(config.getCallback()),
+          OAuthEncoder.encode(config.getScope()));
+    } else {
+      return String.format(AUTHORIZE_URL, config.getApiKey(),
+          OAuthEncoder.encode(config.getCallback()));
     }
   }
 }
